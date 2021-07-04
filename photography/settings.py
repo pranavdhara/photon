235c218ec14c6880e76c8b3b063bdb4561c53ed9
pranavdhara <dharapranav4@gmail.com>
2021-07-04 18:05:55 +0530

    heroku deployment

--- photography/settings.py
+++ photography/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import django_heroku
 
 from pathlib import Path
 
@@ -142,3 +143,8 @@ EMAIL_HOST_USER = 'pranavdhara6@gmail.com'
 EMAIL_HOST_PASSWORD = 'kjhrdysnqwexqvgw' 
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True 
+
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
